bae8f855690b02ec7594aae0426018ee0320002f
Anshul Pagar <anshulpagar@gmail.com>
2026-06-10 09:32:05 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,54 @@
+import { useState } from "react";
+
 import CitizenBooking from "./pages/CitizenBooking";
 
+import StaffLogin from "./pages/staff/StaffLogin";
+import StaffDashboard from "./pages/staff/StaffDashboard";
+import Appointments from "./pages/staff/Appointments";
+
 function App() {
-  return <CitizenBooking />;
+  const [page, setPage] = useState("citizen");
+
+  return (
+    <div>
+      <div
+        style={{
+          padding: "10px",
+          display: "flex",
+          gap: "10px",
+          background: "#e5e7eb"
+        }}
+      >
+        <button onClick={() => setPage("citizen")}>
+          Citizen Portal
+        </button>
+
+        <button onClick={() => setPage("staff-login")}>
+          Staff Login
+        </button>
+
+        <button onClick={() => setPage("staff-dashboard")}>
+          Staff Dashboard
+        </button>
+
+        <button onClick={() => setPage("appointments")}>
+          Appointments
+        </button>
+      </div>
+
+      {page === "citizen" && <CitizenBooking />}
+
+      {page === "staff-login" && <StaffLogin />}
+
+      {page === "staff-dashboard" && (
+        <StaffDashboard />
+      )}
+
+      {page === "appointments" && (
+        <Appointments />
+      )}
+    </div>
+  );
 }
 
 export default App;
