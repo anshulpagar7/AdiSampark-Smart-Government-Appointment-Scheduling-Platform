7ab1d44472ba7c1aaadbaef8fd9a213081100fab
Anshul Pagar <anshulpagar@gmail.com>
2026-07-02 01:12:36 +0530

    Update index.js

--- google-auth/index.js
+++ google-auth/index.js
@@ -3,8 +3,8 @@ const express = require("express");
 const open = require("open").default;
 const { google } = require("googleapis");
 
-const CLIENT_ID = "286050563294-vlf0479drktu2o46u77ggte1ak9n8fjp.apps.googleusercontent.com";
-const CLIENT_SECRET = "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";
+const CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
 const REDIRECT_URI = "http://localhost:3000/oauth2callback";
 
 const oauth2Client = new google.auth.OAuth2(
